5ace61041136f9a2afacb86273a7127ba1a1b46f
Weslley Neri <weslley39@gmail.com>
2014-07-08 13:36:38 -0300

    gitignore added

--- api-config.js
+++ api-config.js
@@ -3,5 +3,5 @@
  */
 window.api = 'https://api.themoviedb.org/3';
 window.api_images = 'http://image.tmdb.org/t/p/w500';
-window.api_key = 'c0eacd1527a5ec8b6897e491f294f20e';
+
 window.debug = true;
---
+++ key.js
@@ -0,0 +1,4 @@
+/**
+ * Created by weslley on 08/07/14.
+ */
+window.api_key = 'c0eacd1527a5ec8b6897e491f294f20e';
